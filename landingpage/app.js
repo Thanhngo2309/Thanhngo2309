394acb99dd321fa6a66236ca7c27a6ba4e6ef0fb
HoangThiThaoNhi <you@example.com>
2024-05-28 14:24:07 +0700

    Setting Darkmode in Home

--- landingpage/app.js
+++ landingpage/app.js
@@ -4,3 +4,16 @@ window.addEventListener("scroll",function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 })
 
+const toggleTheme = document.querySelector('.toggle-theme');
+const body = document.body;
+
+toggleTheme.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+
+  // Update the icon based on the current theme
+  if (body.classList.contains('dark-mode')) {
+    toggleTheme.innerHTML = '<i class="fas fa-moon"></i>';
+  } else {
+    toggleTheme.innerHTML = '<i class="fas fa-sun"></i>';
+  }
+});
